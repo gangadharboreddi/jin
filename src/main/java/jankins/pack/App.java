76238b9a30b79ee7db3fbac43abf7092753cde5d
ram <ram@gmail.com>
2022-05-11 17:14:52 +0530

    test files

---
+++ src/main/java/jankins/pack/App.java
@@ -0,0 +1,7 @@
+package jankins.pack;
+
+public class App {
+    public static String getHello() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/jankins/pack/AppTest.java
@@ -0,0 +1,9 @@
+package jankins.pack;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+    public void testGetHello() throws Exception {
+        assertEquals("Hello", App.getHello());
+    }
+}
